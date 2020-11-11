cdcc23dd1162611f4cac66a32e5904fdf21443ca
lobbato <emily.lobbato@gmail.com>
2020-11-10 18:14:59 -0800

    Still flowndering but trying

---
+++ Java/scripts.js
@@ -0,0 +1,20 @@
+function add(number1, number2) {
+    return number1 + number2;
+  }
+  
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+  const result = add(number1, number2);
+  alert(result);
+function saySomething(whatToSay) {
+    alert(whatToSay);
+  }
+  
+  function add(number1, number2) {
+    return number1 + number2;
+  }
+  
+  saySomething("The sum is " + add(3,5) + ".");
+
+  function add(number1, number2) {
+    return number1 + number2;
