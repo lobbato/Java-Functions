28f6b836c06fdc8c4e0acfdb08ce352493b8c177
lobbato <emily.lobbato@gmail.com>
2020-11-10 19:32:42 -0800

    java changes

--- Java/scripts.js
+++ Java/scripts.js
@@ -1,15 +1,13 @@
-function add(number1, number2) {
-    return number1 + number2;
-}
-jQuery("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!");
-   });
+jQuery(document).ready(function() {
+    jQuery("h1").click(function() {
+      alert("This is a header.");
+    });
   
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
+    jQuery("p").click(function() {
+      alert("This is a paragraph.");
+    });
   
-  jQuery("img").click(function() {
-    alert("This is an image.");
+    jQuery("img").click(function() {
+      alert("This is an image.");
+    });
   });
